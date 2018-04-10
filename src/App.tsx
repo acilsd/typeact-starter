8c8720db42be01e88cbd809090ce4afccbadd5b0
acilsd <afrolov@phoenixit.ru>
2018-04-10 17:27:56 +0300

    fix es6 modules resolution, add styled-components

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,20 @@
 import * as React from 'react';
+import styled from 'styled-components';
 import { hot } from 'react-hot-loader';
 
+const StyledDiv = styled('div')`
+  width: 100%;
+  height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  text-align: center;
+`;
+
 const App = () => (
-  <h1>
-    Hello, anon
-  </h1>
+  <StyledDiv>
+    <p>Hello world</p>
+  </StyledDiv>
 );
 
 export default hot(module)(App);
--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,11 @@
 import * as React from 'react';
 import { render } from 'react-dom';
 import App from './App';
+import { injectGlobal } from 'styled-components';
+
+injectGlobal`
+    * { margin: 0; padding: 0; }
+`;
 
 const root = document.getElementById('root');
 
